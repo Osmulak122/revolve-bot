f1dc637548dc3dc00960087b0399e710cb131251
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-03-25 16:08:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ bot.on("message", function(message) {
     var channel1 = bot.channels.find('name', 'readme');
     if (message.channel == channel1 && message.content == "!agree") {
         var role_agree = message.member.guild.roles.find('name', 'Unaccepted Rules');
-        var msgauthor = message.author;
         message.delete();
         message.member.removeRole(role_agree);
         message.author.send("**Verification Completed!**\n*Welcome to the server and have fun!*");
@@ -38,13 +37,16 @@ bot.on("message", function(message) {
         
     
     if (message.content == "!help") {
-        message.channel.send("__**Commands :**__\n\n**!leaders**");
+        message.channel.send("__**Commands :**__\n\n**!leaders**\n**!botcode**");
     }                    
     
     if(message.content == "!leaders") {
         message.channel.send("Leaders :\n**Four** :flag_es:\n**Liptik** :flag_pt:\nCo-Leaders\n**Minty** :flag_pl:\n**Yanick** :flag_nl:");
     }
     
+    if(message.content == "!botcode") {
+        message.channel.send("**Actual bot code!** - <https://github.com/Osmulak122/revolve-bot/master/index.js>");
+    }
 
 
 
