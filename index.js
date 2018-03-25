b9d92600624272e07b63f6870b6baaa26aafab31
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-03-25 15:50:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,14 @@ bot.on("ready", function() {
 bot.on("message", function(message) {
 
     var channel1 = bot.channels.find('name', 'readme');
+    var channel_logs = bot.channel.find('name', 'bot_logs');
     if (message.channel == channel1 && message.content == "!agree") {
         var role_agree = message.member.guild.roles.find('name', 'Unaccepted Rules');
+        var msgauthor = message.author;
         message.delete();
         message.member.removeRole(role_agree);
         message.author.send("**Verification Completed!**\n*Welcome to the server and have fun!*");
+        message.channel(channel_logs).send.msgauthor.toString() + ", *Agreed to Rules!*");
     }
   
     var channel1 = bot.channels.find('name', 'readme');
